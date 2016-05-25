0e7234b7477aa29af3948d6f178212b97372bf08
Illia Syniuhin <syniuhin@gmail.com>
2016-05-25 20:59:58 +0300

    Changed trigger to check if relationship exists.

--- lab02/db.py
+++ lab02/db.py
@@ -68,14 +68,25 @@ def create_photo_trigger():
   with con:
     cur = con.cursor()
     cur.execute("""
-                CREATE TRIGGER ins_relations AFTER INSERT ON `lab02db`.`lab02_photo` 
-                FOR EACH ROW 
-                BEGIN 
-                	IF NEW.photographer_id IS NOT NULL THEN 
-                		INSERT INTO `lab02db`.`lab02_photographer_cameras`(photographer_id, camera_id) VALUES(NEW.photographer_id, NEW.camera_id); 
-                   INSERT INTO `lab02db`.`lab02_photographer_locations`(photographer_id, location_id) VALUES(NEW.photographer_id, NEW.location_id); 
-                	END IF; 
-                END; """)
+                CREATE TRIGGER ins_relations AFTER INSERT ON `lab02db`.`lab02_photo`
+                FOR EACH ROW
+                BEGIN
+                  IF NEW.photographer_id IS NOT NULL THEN
+                    SET @ph_cam = (SELECT id from lab02_photographer_cameras WHERE
+                        photographer_id = NEW.photographer_id AND camera_id = NEW.camera_id);
+                    IF @ph_cam IS NULL THEN
+                      INSERT INTO `lab02db`.`lab02_photographer_cameras`(photographer_id,
+                          camera_id) VALUES(NEW.photographer_id, NEW.camera_id);
+                    END IF;
+                      SET @ph_loc = (SELECT id from lab02_photographer_locations WHERE
+                        photographer_id = NEW.photographer_id AND location_id = NEW.location_id);
+                    IF @ph_loc IS NULL THEN
+                      INSERT INTO `lab02db`.`lab02_photographer_locations`(photographer_id,
+                          location_id) VALUES(NEW.photographer_id, NEW.location_id);
+                    END IF;
+                  END IF;
+                END;
+                """)
 
 
 def drop_photo_trigger():
--- lab03/trigger_definition.sql
+++ lab03/trigger_definition.sql
@@ -5,12 +5,25 @@ delimiter //
 CREATE TRIGGER ins_relations AFTER INSERT ON `lab02db`.`lab02_photo`
 FOR EACH ROW
 BEGIN
-	IF @trigger_disabled IS NULL AND NEW.photographer_id IS NOT NULL THEN
-		INSERT INTO `lab02db`.`lab02_photographer_cameras`(photographer_id, camera_id) VALUES(NEW.photographer_id, NEW.camera_id);
-        INSERT INTO `lab02db`.`lab02_photographer_locations`(photographer_id, location_id) VALUES(NEW.photographer_id, NEW.location_id);
+	IF NEW.photographer_id IS NOT NULL THEN
+		SET @ph_cam = (SELECT id from lab02_photographer_cameras WHERE
+				photographer_id = NEW.photographer_id AND camera_id = NEW.camera_id);
+		IF @ph_cam IS NULL THEN
+			INSERT INTO `lab02db`.`lab02_photographer_cameras`(photographer_id,
+					camera_id) VALUES(NEW.photographer_id, NEW.camera_id);
+		END IF;
+   		SET @ph_loc = (SELECT id from lab02_photographer_locations WHERE
+				photographer_id = NEW.photographer_id AND location_id = NEW.location_id);
+		IF @ph_loc IS NULL THEN
+			INSERT INTO `lab02db`.`lab02_photographer_locations`(photographer_id,
+					location_id) VALUES(NEW.photographer_id, NEW.location_id);
+		END IF;
 	END IF;
 END;
 //
 delimiter ;
 
 SHOW TRIGGERS LIKE 'lab02_photo';
+
+SELECT * from lab02_photographer_cameras WHERE
+		photographer_id = 78 AND camera_id = 94;
